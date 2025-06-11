6008d90ad5a50f2c16e95e2a03d0858c90fcff89
bookmyassets <bookmyassets101@gmail.com>
2025-06-11 12:09:54 +0530

    chnages

--- src/app/components/CommonForm.jsx
+++ src/app/components/CommonForm.jsx
@@ -108,7 +108,7 @@ export default function CommonForm({ title }) {
       const now = Date.now();
 
       const response = await fetch(
-        "https://api.telecrm.in/enterprise/<workspace-id>/autoupdatelead",
+        "https://api.telecrm.in/enterprise/67a30ac2989f94384137c2ff/autoupdatelead",
         {
           method: "POST",
           headers: {
