9bd0331bcb7c6a4a299dc7818590210e34ae965b
bookmyassets <bookmyassets101@gmail.com>
2025-10-01 10:52:35 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "./components/header";
 
 import ThemeClientProvider from "./components/ThemeProvider";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 export const metadata = {
   title: "Dholera Insider",
@@ -13,6 +14,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" >
+
+<head>
+
+<Script type="text/javascript">
+
+{`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tj7p027fn5");
+`}
+
+</Script>
+
+</head>
+
       <body>
         
           <Header />
