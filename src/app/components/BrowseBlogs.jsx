141a2270cdb7d4c8fa720a48dafc4644496e7823
bookmyassets <bookmyassets101@gmail.com>
2025-10-03 11:10:11 +0530

    changes

--- src/app/components/BrowseBlogs.jsx
+++ src/app/components/BrowseBlogs.jsx
@@ -135,7 +135,7 @@ export default function LatestUpdates() {
         </p>
       </div>
       
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {loading
           ? Array(4)
               .fill(0)
